5998c50f4dc6dbef44e9eeb3d65bc73aa7a5b056
Samuel CALEGARI <samuel.calegari@gmail.com>
2017-06-12 11:07:05 +0200

    initial

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+function resizeText(multiplier) {
+    if (document.body.style.fontSize == "") {
+        document.body.style.fontSize = "1.0em";
+    }
+    document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.1) + "em";
+}
+
 $(function() {
 
     $("#block_accessplus_decrease_font").on('click', function(){
-        alert('decrease font');
+        resizeText(-1);
     })
 
     $("#block_accessplus_increase_font").on('click', function(){
-        alert('increase font');
+        resizeText(1);
     })
+
 });
